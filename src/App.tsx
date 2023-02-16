fd6585780db047530552f85be754940024e224d0
MoAtefAmer <mo.atefamer@gmail.com>
2023-02-16 16:06:50 +0200

    chore: first commit (tailwind install)

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+  <div>
+    <h1 className="text-3xl font-bold underline p-3 text-blue-500">
+      Hello world!
+    </h1>
+
+  </div>
+  )
+}
+
+export default App
